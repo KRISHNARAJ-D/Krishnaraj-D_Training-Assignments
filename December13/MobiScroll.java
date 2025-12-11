cb84c6398a404caaaf95be4036025ff8eb9d1c02
KRISHNARAJ D <119559695+KRISHNARAJ-D@users.noreply.github.com>
2025-12-11 20:28:38 +0530

    Add files via upload

---
+++ December13/MobiScroll.java
@@ -0,0 +1,42 @@
+package seleniumprogram;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class MobiScroll{
+
+    @Test
+    public void MultipleDropTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        
+        driver.get("https://demo.mobiscroll.com/jquery/select/multiple-select");
+        driver.findElement(By.cssSelector("#multiple-select-context > label > span.mbsc-ios.mbsc-ltr.mbsc-textfield-inner.mbsc-textfield-inner-outline.mbsc-textfield-inner-stacked.mbsc-textarea-inner.mbsc-textfield-tags-inner > span.mbsc-textfield-tags.mbsc-ios.mbsc-ltr.mbsc-textfield.mbsc-textfield-outline.mbsc-select.mbsc-textfield-stacked.mbsc-textfield-outline-stacked.mbsc-textarea > span")).click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(
+        	    By.xpath("//div[@role='option' and text()='Books']/span")
+        	)).click();
+        
+        wait.until(ExpectedConditions.elementToBeClickable(
+        	    By.xpath("//div[@role='option' and text()='Movies, Music & Games']/span")
+        	)).click();
+        
+        wait.until(ExpectedConditions.elementToBeClickable(
+        	    By.xpath("//div[@role='option' and text()='Electronics & Computers']/span")
+        	)).click();
+
+
+
+    }
+}
+
